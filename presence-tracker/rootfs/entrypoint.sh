d8001faf9d8169eb43e85207c66501768a6a3abb
Julien Kassar <code@juli3n.xyz>
2024-07-13 08:11:50 +0200

    Update presence-tracker addon

--- presence-tracker/rootfs/entrypoint.sh
+++ presence-tracker/rootfs/entrypoint.sh
@@ -5,43 +5,50 @@
 PT_CONFIG_PATH="/etc/presence-tracker"
 
 
-debug=$(bashio::config 'debug')
-state_path=$(bashio::config 'state_path')
+debug="$(bashio::config 'debug')"
+state_path="$(bashio::config 'state_path')"
+mqtt_enabled="$(bashio::config 'mqtt.enabled')"
+mqtt_host=""
+mqtt_port=""
+mqtt_username=""
+mqtt_password=""
 
-if [ ! -d "$state_path" ]; then
-	mkdir -p "$state_path"
-fi
+mkdir -p "$state_path"
 
-if bashio::config.true 'mqtt.autodiscover'; then
+if bashio::config.true 'mqtt.enabled'); then
+  if bashio::config.true 'mqtt.autodiscover'; then
     bashio::log.info "mqtt.autodiscover is defined in options, attempting autodiscovery..."
-	# Check if mqtt service available
-	if ! bashio::services.available "mqtt"; then bashio::exit.nok "No internal MQTT service found. Please install Mosquitto broker"; fi
+    if ! bashio::services.available "mqtt"; then
+      bashio::exit.nok "No internal MQTT service found. Please install Mosquitto broker"
+    fi
 
-	bashio::log.info "... MQTT service found, fetching server detail ..."
-	mqtt_host=$(bashio::services mqtt "host")
-	mqtt_port=$(bashio::services mqtt "port")
-	mqtt_username=$(bashio::services mqtt "username")
-	mqtt_password=$(bashio::services mqtt "password")
-else
-	mqtt_host=$(bashio::config 'mqtt.host')
-	mqtt_port=$(bashio::config 'mqtt.port')
-	mqtt_username=$(bashio::config 'mqtt.username')
-	mqtt_password=$(bashio::config 'mqtt.password')
+    bashio::log.info "... MQTT service found, fetching server detail ..."
+    mqtt_host="$(bashio::services mqtt "host")"
+    mqtt_port="$(bashio::services mqtt "port")"
+    mqtt_username="$(bashio::services mqtt "username")"
+    mqtt_password="$(bashio::services mqtt "password")"
+  else
+    mqtt_host="$(bashio::config 'mqtt.host')"
+    mqtt_port="$(bashio::config 'mqtt.port')"
+    mqtt_username="$(bashio::config 'mqtt.username')"
+    mqtt_password="$(bashio::config 'mqtt.password')"
+  fi
 fi
 
 bashio::var.json \
-	state_path $state_path \
-	mqtt_host $mqtt_host \
-	mqtt_port $mqtt_port \
-	mqtt_username $mqtt_username \
-	mqtt_password $mqtt_password \
+	state_path "$state_path" \
+  mqtt_enabled "$mqtt_enabled" \
+	mqtt_host "$mqtt_host" \
+	mqtt_port "$mqtt_port" \
+	mqtt_username "$mqtt_username" \
+	mqtt_password "$mqtt_password" \
 	| tempio \
-		-template ${PT_CONFIG_PATH}/templates/config.gtpl \
-		-out ${PT_CONFIG_PATH}/config.hcl
+		-template "${PT_CONFIG_PATH}/templates/config.gtpl" \
+		-out "${PT_CONFIG_PATH}/config.hcl"
 
 
 bashio::log.info "Starting the app"
 /usr/local/bin/presence-tracker \
-	-config ${PT_CONFIG_PATH}/config.hcl \
-	-debug $debug \
+	-config "${PT_CONFIG_PATH}/config.hcl" \
+	-debug "$debug" \
 	|| bashio::log.fatal "The app has crashed. Are you sure you entered the correct config options?"
